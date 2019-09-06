ceb2b7d31375f0437aff130051f90d56e41011a1
Domovoi-Kuzma <kosiak10851@gmail.com>
2019-09-06 12:52:19 +0300

    убраны лишние комменты

--- protected/models/Meets.php
+++ protected/models/Meets.php
@@ -131,7 +131,6 @@ class Meets extends CActiveRecord
     /**
      * переворот лайка (если есть - удаление, если нет - добавление)
      * вывод массива результатов в документ для ajax в виде текста, разделённого @
-     * @todo разузнать, стоит ли вынести вывод в отдельный метод модели или вообще в новый вид.
      * @author  Sasha
      * @data    04.09.2019
      */
@@ -144,7 +143,6 @@ class Meets extends CActiveRecord
         $criteria->params=array(':user_crit'=>Yii::app()->user->id, ':meet_crit'=>$this->ID);
         $result=Like::model()->find($criteria);
         if (is_null($result)) {
-            //insert user's <like> here
             $model = new Like;
             $model->user_id = Yii::app()->user->id;
             $model->meet_id = $this->ID;
--- protected/views/site/viewLikeButton.php
+++ protected/views/site/viewLikeButton.php
@@ -1,18 +1,4 @@
 <?php
-/*
- * <script>
-
-    function testDivSwap(elem) {
-        elem.innerHTML="<div class='like_button_icon' style=\"--picture:url('no_heart.png')\"><span>понравилось после клика</span></div>";
-    }
-</script>
-<div onClick='testDivSwap(this);'>
-<div class='like_button_icon' style="--picture:url('heart.png')">
-    <span>Понравилось Саше</span>
-    <span>Понравилось Максиму</span>
-</div>
-</div>
- */
 
 $likeParam=$model->getLikeStatus();
 echo "<div class='button_icon' style='--picture: var(--".$likeParam['icon'].")' >";
